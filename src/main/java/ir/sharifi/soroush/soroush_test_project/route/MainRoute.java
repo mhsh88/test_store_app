0a5dc250f7c618eb5f6d03cd3c8c9e069350b954
hossein sharifi <hosseinsharifi@hotmail.com>
2020-10-08 22:47:24 +0330

    add code

--- src/main/java/ir/sharifi/soroush/soroush_test_project/route/MainRoute.java
+++ src/main/java/ir/sharifi/soroush/soroush_test_project/route/MainRoute.java
@@ -1,13 +1,17 @@
 package ir.sharifi.soroush.soroush_test_project.route;
 
 
-import ir.sharifi.soroush.soroush_test_project.model.ResponseType;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.soroushbot.models.MinorType;
 import org.apache.camel.component.soroushbot.models.SoroushMessage;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
 
 @Component
 @Slf4j
@@ -28,6 +32,28 @@ public class MainRoute extends RouteBuilder {
                 .process(exchange -> {
                     SoroushMessage soroushMessage = exchange.getIn().getBody(SoroushMessage.class);
                     soroushMessage.setTo(soroushMessage.getFrom());
+                    if(Pattern.matches("http(?:s?):\\/\\/(?:www\\.)?youtu(?:be\\.com\\/watch\\?v=|\\.be\\/)([\\w\\-\\_]*)(&(amp;)?\u200C\u200B[\\w\\?\u200C\u200B=]*)?",soroushMessage.getBody())) {
+                        String command = "youtube-dl -f 22 --proxy socks5://127.0.0.1:9050/ " + soroushMessage.getBody();
+
+                        Process proc = Runtime.getRuntime().exec(command);
+
+                        // Read the output
+
+                        BufferedReader reader =
+                                new BufferedReader(new InputStreamReader(proc.getInputStream()));
+
+                        String line = "";
+                        StringBuilder st = new StringBuilder();
+                        while ((line = reader.readLine()) != null) {
+                            log.info(line);
+                            st.append(line).append("\n");
+                        }
+
+                        proc.waitFor();
+                        soroushMessage.setType(MinorType.TEXT);
+                        soroushMessage.setBody(st.toString());
+
+                    }
                 }).to(producer);
 
         rest("/")
