0162913c2e73cd6cbe8bf901c132f1c7e72a2ee9
hossein sharifi <hosseinsharifi@hotmail.com>
2020-11-15 01:57:49 +0330

    add upload rate

--- src/main/java/ir/sharifi/soroush/soroush_test_project/route/MainRoute.java
+++ src/main/java/ir/sharifi/soroush/soroush_test_project/route/MainRoute.java
@@ -10,12 +10,15 @@ import org.apache.camel.component.soroushbot.models.FileType;
 import org.apache.camel.component.soroushbot.models.MinorType;
 import org.apache.camel.component.soroushbot.models.SoroushMessage;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Objects;
 import java.util.concurrent.Executors;
@@ -28,6 +31,7 @@ import static org.apache.http.entity.ContentType.MULTIPART_FORM_DATA;
 @Slf4j
 public class MainRoute extends RouteBuilder {
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private static final DecimalFormat df2 = new DecimalFormat("#.##");
 
     @Value("${soroush.bot.consumer}")
     private String consumer;
@@ -42,7 +46,7 @@ public class MainRoute extends RouteBuilder {
                 .process(exchange -> {
                     Exception exception = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Exception.class);
                     String header = exchange.getIn().getHeader(Exchange.FILE_NAME, String.class);
-                    if(Objects.nonNull(header) && !header.isEmpty()){
+                    if (Objects.nonNull(header) && !header.isEmpty()) {
                         File file = new File(header);
                         file.delete();
                     }
@@ -62,43 +66,24 @@ public class MainRoute extends RouteBuilder {
 //                        String command = "youtube-dl -f 22 --proxy socks5://localhost:9050 " + soroushMessage.getBody();
 
                         ProcessBuilder builder = new ProcessBuilder();
-//                        builder.command("youtube-dl", "-f", "18", "--proxy", "socks5://localhost:9050", "--get-filename", soroushMessage.getBody());
-                        builder.command("youtube-dl", "-f", "18", "--get-filename", soroushMessage.getBody());
+                        builder.command("youtube-dl", "-f", "18", "--proxy", "socks5://localhost:9050", "--get-filename", soroushMessage.getBody());
+//                        builder.command("youtube-dl", "-f", "22", "--get-filename", soroushMessage.getBody());
 //                            builder.command(command);
 //                        builder.directory(new File(System.getProperty("user.home")));
                         Process process = builder.start();
 
                         String fileName = new BufferedReader(new InputStreamReader(process.getInputStream())).lines().findFirst().orElse("");
 
-//                        builder.command("youtube-dl", "-f", "18", "--proxy", "socks5://localhost:9050", soroushMessage.getBody());
-                        builder.command("youtube-dl", "-f", "18", soroushMessage.getBody());
+                        builder.command("youtube-dl", "-f", "18", "--proxy", "socks5://localhost:9050", soroushMessage.getBody());
+//                        builder.command("youtube-dl", "-f", "22", soroushMessage.getBody());
                         process = builder.start();
                         StreamGobbler streamGobbler =
                                 new StreamGobbler(process.getInputStream(), log::info);
                         Executors.newSingleThreadExecutor().submit(streamGobbler);
                         int exitCode = process.waitFor();
                         assert exitCode == 0;
-//
-//                        Process proc = Runtime.getRuntime().exec(command);
-//
-//                        // Read the output
-//
-//                        BufferedReader reader =
-//                                new BufferedReader(new InputStreamReader(proc.getInputStream()));
-//
-//                        String line = "";
-//                        StringBuilder st = new StringBuilder();
-//                        while ((line = reader.readLine()) != null) {
-//                            log.info(line);
-//                            st.append(line).append("\n");
-//                        }
-//
-//                        proc.waitFor();
-//                        st.append(System.getProperty("os.name")).append("\n");
-//                        st.append(System.getProperty("os.arch")).append("\n");
-//                        st.append(System.getProperty("os.version")).append("\n");
+
                         soroushMessage.setType(MinorType.TEXT);
-//                        soroushMessage.setBody(st.toString());
                         soroushMessage.setBody(fileName);
                         exchange.getIn().setHeader(Exchange.FILE_NAME, fileName);
                         exchange.getIn().setHeader("custom_header", soroushMessage);
@@ -108,22 +93,127 @@ public class MainRoute extends RouteBuilder {
                 .process(exchange -> {
                     MultipartEntityBuilder multipartEntityBuilder =
                             MultipartEntityBuilder.create();
-//                    multipartEntityBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
 
                     String filename = (String)
                             exchange.getIn().getHeader(Exchange.FILE_NAME);
 
-//                    File file = exchange.getIn().getBody(File.class);
                     File file = new File(filename);
-//                    exchange.getIn().setHeader("custom_file_object",file);
-                    if(!file.exists()){
+                    long totalSize = file.length();
+                    if (!file.exists()) {
                         throw new FileNotFoundException("file not found");
                     }
 
                     multipartEntityBuilder.addPart("file",
                             new FileBody(file, MULTIPART_FORM_DATA, filename));
 
-                    exchange.getIn().setBody(multipartEntityBuilder.build());
+                    HttpEntity yourEntity = multipartEntityBuilder.build();
+
+                    class ProgressiveEntity implements HttpEntity {
+                        @Override
+                        public void consumeContent() throws IOException {
+                            yourEntity.consumeContent();
+                        }
+
+                        @Override
+                        public InputStream getContent() throws IOException,
+                                IllegalStateException {
+                            return yourEntity.getContent();
+                        }
+
+                        @Override
+                        public Header getContentEncoding() {
+                            return yourEntity.getContentEncoding();
+                        }
+
+                        @Override
+                        public long getContentLength() {
+                            return yourEntity.getContentLength();
+                        }
+
+                        @Override
+                        public Header getContentType() {
+                            return yourEntity.getContentType();
+                        }
+
+                        @Override
+                        public boolean isChunked() {
+                            return yourEntity.isChunked();
+                        }
+
+                        @Override
+                        public boolean isRepeatable() {
+                            return yourEntity.isRepeatable();
+                        }
+
+                        @Override
+                        public boolean isStreaming() {
+                            return yourEntity.isStreaming();
+                        } // CONSIDER put a _real_ delegator into here!
+
+                        @Override
+                        public void writeTo(OutputStream outstream) throws IOException {
+
+                            class ProxyOutputStream extends FilterOutputStream {
+
+                                public ProxyOutputStream(OutputStream proxy) {
+                                    super(proxy);
+                                }
+
+                                public void write(int idx) throws IOException {
+                                    out.write(idx);
+                                }
+
+                                public void write(byte[] bts) throws IOException {
+                                    out.write(bts);
+                                }
+
+                                public void write(byte[] bts, int st, int end) throws IOException {
+                                    out.write(bts, st, end);
+                                }
+
+                                public void flush() throws IOException {
+                                    out.flush();
+                                }
+
+                                public void close() throws IOException {
+                                    out.close();
+                                }
+                            }
+
+                            class ProgressiveOutputStream extends ProxyOutputStream {
+                                long totalSent;
+                                final long startTime;
+                                long endTime;
+
+
+                                public ProgressiveOutputStream(OutputStream proxy) {
+                                    super(proxy);
+                                    totalSent = 0;
+                                    startTime = System.currentTimeMillis();
+                                }
+
+                                public void write(byte[] bts, int st, int end) throws IOException {
+
+
+                                    totalSent += end;
+                                    endTime = System.currentTimeMillis();
+                                    double megaByte = totalSent * 1.0 / 1024 / 1024;
+                                    log.info("upload file progress: {} mb {} kb/s overall speed {} % uploaded",
+                                            df2.format(megaByte),
+                                            df2.format(megaByte * 1000 * 1024 * 1.0 /(endTime - startTime)),
+                                            df2.format((totalSent * 1.0 / totalSize) * 100));
+
+
+                                    out.write(bts, st, end);
+                                }
+                            }
+
+                            yourEntity.writeTo(new ProgressiveOutputStream(outstream));
+                        }
+
+                    }
+                    ProgressiveEntity myEntity = new ProgressiveEntity();
+                    exchange.getIn().setBody(myEntity);
                 })
 
                 .setHeader(Exchange.HTTP_METHOD, constant("POST"))
@@ -138,7 +228,7 @@ public class MainRoute extends RouteBuilder {
                     String filename = (String)
                             exchange.getIn().getHeader(Exchange.FILE_NAME);
                     File file = new File(filename);
-                    if(botFileResponse.getResultCode() == 200){
+                    if (botFileResponse.getResultCode() == 200) {
                         soroushMessage.setType(MinorType.FILE);
                         soroushMessage.setFileType(FileType.ATTACHMENT);
                         soroushMessage.setFileUrl(botFileResponse.getFileUrl());
@@ -146,14 +236,15 @@ public class MainRoute extends RouteBuilder {
 
                         soroushMessage.setFileSize((double) file.length());
                         soroushMessage.setTo(soroushMessage.getFrom());
-                    }soroushMessage.setTo(soroushMessage.getFrom());
+                    }
+                    soroushMessage.setTo(soroushMessage.getFrom());
 
 
                     exchange.getIn().setHeaders(new HashMap<>());
                     exchange.getIn().setBody(soroushMessage);
                     try {
                         file.delete();
-                    }catch (Exception ignored){
+                    } catch (Exception ignored) {
 
                     }
 
@@ -176,8 +267,8 @@ public class MainRoute extends RouteBuilder {
 }
 
 class StreamGobbler implements Runnable {
-    private InputStream inputStream;
-    private Consumer<String> consumer;
+    private final InputStream inputStream;
+    private final Consumer<String> consumer;
 
     public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
         this.inputStream = inputStream;
